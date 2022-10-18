2aa4edf2bcaaea5363fa21814346b468f0d8ec5a
Guilherme Alcântara <gui.absilva@outlook.com>
2022-10-17 22:28:47 -0300

    first commit

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,15 @@
+import { styled } from '..'
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  minHeight: '100vh'
+})
+
+export const Header = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: "0 auto"
+})
